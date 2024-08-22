67585b2426a495c24e964d81200101203e17e9d5
Quy Duc <quyduc711@gmail.com>
2024-08-22 17:11:02 +0200

    init project

---
+++ gymaite/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  gymaite
+//
+//  Created by Quý Đức on 22/08/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
